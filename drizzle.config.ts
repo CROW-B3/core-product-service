f4a740b87a3a06afbf25a77f3c864a466f4bf4f4
Ranuga Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2026-01-08 21:45:21 +0530

    feat: add -local D1 database ID for remote drizzle studio

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,6 +9,7 @@ export default drizzleD1Config(
   {
     accountId: process.env.CLOUDFLARE_D1_ACCOUNT_ID,
     apiToken: process.env.CLOUDFLARE_D1_API_TOKEN,
+    databaseId: '28bfe4ff-4126-416b-a65e-bd9740184ff3',
     binding: 'DB',
     remote: process.env.REMOTE === 'true' || process.env.REMOTE === '1',
   }
